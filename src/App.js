d2601561f1d410c81dd3936a338f8e55490c9348
shiridi kumar <shiridikumar@shiridis-MacBook-Pro.local>
2024-06-05 12:19:24 +0530

    updated ui

--- src/App.js
+++ src/App.js
@@ -8,14 +8,19 @@ import {
   createBrowserRouter,
 } from "react-router-dom";
 import HomeScreen from './components/HomeScreen';
+import PersistentDrawerLeft from './components/Dummy';
+
+// import PersistentDrawerLeft from './components/Dummy';
+// import PersistentDrawerLeft from './components/D';
 
 function App() {
+
   return (
     <div className="App">
        <BrowserRouter>
         <Routes>
-          <Route exact path="/" element={<HomeScreen/>}/>
-          {/* <Route exact path="/dashboard" element={<Dashboard />} /> */}
+          <Route exact path="/new" element={<PersistentDrawerLeft/>}/>
+          <Route exact path="/" element={<HomeScreen />} />
           {/* <Route exact path="/signup" element={<SignUp />} /> */}
 
         </Routes>
--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useCallback } from 'react';
 import ReactFlow, {
     MiniMap,
@@ -13,6 +13,8 @@ import ReactFlow, {
 import 'reactflow/dist/style.css';
 import "../main.css"
 import DataObjectIcon from '@mui/icons-material/DataObject';
+import FileUploadIcon from '@mui/icons-material/FileUpload';
+import CloseIcon from '@mui/icons-material/Close';
 
 const node1 = ({ data }) => {
     return (
@@ -64,25 +66,59 @@ export default function HomeScreen() {
         "custom": node1,
         "Dataset": node2
     }
+    // useState
+
 
     return (
-        <div style={{ width: '100vw', height: '100vh' }}>
-            {/* <div>
-                <h1> Hello akanksha</h1>
-                <i class="fa-solid fa-database fa-lg" style="color: #FFD43B;"></i>
-            </div> */}
-            <ReactFlow
-                nodes={nodes}
-                edges={edges}
-                onNodesChange={onNodesChange}
-                onEdgesChange={onEdgesChange}
-                onConnect={onConnect}
-                nodeTypes={nodetypes}
-            >
+    <>
+        <nav class="navbar navbar-dark bg-dark">
+            <h3>Menu</h3>
+        </nav>
+       
+        <div style={{ width: '100vw', height: '90vh', display:"flex",flexDirection:"row" }}>
+
+            <div className='sidebar' style={{width:"30%",backgroundColor:"#f9f9fa"}}>
+                <hr/>
+                <div className='Datasets-section' style={{backgroundColor:"rgb(238 243 243)", borderRadius:"10px"}}>
+                    <h2>Datasets</h2>
+                    <button className='btn btn-secondary' style={{display:"flex",justifyContent:"flex-start",margin:"20px" ,width:"200px",backgroundColor:"#2b5377"}}>Add new <FileUploadIcon sx={{marginLeft:"5px"}}/></button>
+                    <div className='Datasets'>
+                        
+                        <div className='data-tabs'>
+                            <img className='dimage' src="https://cdn-icons-png.freepik.com/512/6683/6683734.png" alt="Dummy Image 1"/>
+                            <h4>Dataset 1</h4>
+                        </div>
+                        <div className='data-tabs'>
+                            <img className='dimage' src="https://cdn-icons-png.freepik.com/512/138/138928.png" alt="Dummy Image 2"/>
+                            <h4>Dataset 2</h4>
+                        </div>
+                    </div>
+                </div>
+                <hr/>
+                <div className='Algorithm-section' style={{backgroundColor:"rgb(238 243 243)", borderRadius:"10px", padding:"20px"}}>
+                    <h2>Algorithms</h2>
+                    <div className='data-tabs'>
+                        <DataObjectIcon style={{ fontSize: "100px", color: "green" }} />
+                        <h4>Algorithm 1</h4>
+                    </div>
+                </div>
+                    
+            </div>
+            <div className='maincanvas'>
+                <ReactFlow
+                    nodes={nodes}
+                    edges={edges}
+                    onNodesChange={onNodesChange}
+                    onEdgesChange={onEdgesChange}
+                    onConnect={onConnect}
+                    nodeTypes={nodetypes}
+                >
                 <Background />
                 <Controls />
-            </ReactFlow>
+                </ReactFlow>
+            </div>
 
         </div>
+        </>
     );
 }
